0043116eb6ee71fa3307f954e3674b8bc3cf6008
Mandeep Aulakh <119018981+Mdwag316@users.noreply.github.com>
2023-02-28 08:37:27 -0800

    Update seed.js

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -1,8 +1,7 @@
 const sequelize = require('../config/connection');
-const { User, Blogpost } = require('../models');
+const { User } = require('../models');
 
 const userData = require('./userData.json');
-const BlogpostData = require('./blogpostData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +11,6 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const post of BlogpostData) {
-    await Blogpost.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
 
   process.exit(0);
 };
