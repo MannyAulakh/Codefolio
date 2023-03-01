174dba429f08987520047576bc3076aa49a014c2
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-03-01 12:08:58 -0500

    worked on profile view and routes

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // ADD MODELS
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const { User, Profile } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // #TODO
   } catch (err) {
@@ -11,5 +11,34 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+//router.get("/profile", withAuth, async (req, res) => {
+router.get("/profile", async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Profile }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- Main/models/User.js
+++ Main/models/User.js
@@ -42,13 +42,13 @@ User.init(
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
-      }
+      },
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
