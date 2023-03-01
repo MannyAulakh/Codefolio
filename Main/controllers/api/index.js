aa64709c5451699e96eaee14e49cfcb613876fa7
Mdwag316 <mandeepa98@gmail.com>
2023-03-01 08:47:21 -0800

    added home page and route

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const BlogRoutes = require('./BlogRoutes');
 
 router.use('/users', userRoutes);
 //ADD ADDITIONAL ROUTES
