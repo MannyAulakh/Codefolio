3a2aee9707ad6d4cee893e840906ef252783b195
Liguang Zhang <leezhang97@gmail.com>
2023-03-02 11:22:24 -0500

    comments save

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -2,20 +2,55 @@ const router = require('express').Router();
 const { Post, User, Profile } = require('../models');
 const withAuth = require('../utils/auth');
 
+// // displaying the blogs and 1 comment 
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all Posts and JOIN with user data
+//     // const postData = await Post.findAll({
+//     //   // include: [
+//     //   //   {
+//     //   //     model: User,
+//     //   //     attributes: ['name'],
+//     //   //   },
+//     //   // ],
+//     // });
+
+//     console.log(results); 
+//     const [results, metadata] = await sequelize.query("SELECT post_title, post_content, post_date_created, post_username, comment_content, comment_date_created, username AS comment_username FROM(SELECT post.id AS post_id, user.id AS user_id, title AS post_title, post.content AS post_content, post.date_created AS post_date_created, user.username AS post_username, comment.content AS comment_content, comment.date_created AS comment_date_created, comment.user_id AS comment_user_id FROM post JOIN user ON post.user_id = user.id JOIN comment ON post.id = comment.post_id) AS post_comment_user JOIN user ON post_comment_user.comment_user_id = user.id");
+//     console.log(results); 
+//     // Serialize data so the template can read it
+//     // const Posts = postData.map((post) => post.get({ plain: true }));
+//     // results = results.map((result) => result.get({ plain: true }));
+
+//     // Pass serialized data and session flag into template
+//     res.render('homepage', {
+//       results,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// displaying the blogs and 1 comment 
 router.get('/', async (req, res) => {
   try {
     // Get all Posts and JOIN with user data
     const postData = await Post.findAll({
-      // include: [
-      //   {
-      //     model: User,
-      //     attributes: ['name'],
-      //   },
-      // ],
+    //   // include: [
+    //   //   {
+    //   //     model: User,
+    //   //     attributes: ['name'],
+    //   //   },
+    //   // ],
     });
 
+    // console.log(results);
+    const [results, metadata] = await sequelize.query("SELECT post_title, post_content, post_date_created, post_username, comment_content, comment_date_created, username AS comment_username FROM(SELECT post.id AS post_id, user.id AS user_id, title AS post_title, post.content AS post_content, post.date_created AS post_date_created, user.username AS post_username, comment.content AS comment_content, comment.date_created AS comment_date_created, comment.user_id AS comment_user_id FROM post JOIN user ON post.user_id = user.id JOIN comment ON post.id = comment.post_id) AS post_comment_user JOIN user ON post_comment_user.comment_user_id = user.id");
+    console.log(results);
     // Serialize data so the template can read it
     const Posts = postData.map((post) => post.get({ plain: true }));
+    // results = results.map((result) => result.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', {
@@ -27,27 +62,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/Post/:id', async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
 
-    const post = postData.get({ plain: true });
 
-    res.render('Post', {
-      ...post,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+
+
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
